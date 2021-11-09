92c0465d6c4341ebdbae2048536a786d760d57ed
Mariya <va.mariya@yahoo.com>
2021-11-09 01:45:51 +0000

    Filling Out the urls_index.ejs Template

--- express_server.js
+++ express_server.js
@@ -6,12 +6,6 @@ app.set("view engine", "ejs");
 
 
 
-
-
-const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -31,4 +25,9 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
