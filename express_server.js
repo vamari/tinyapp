a552be24fe01aa4f5f2f2ca40e409a35d01156f1
Mariya <va.mariya@yahoo.com>
2021-11-11 23:08:53 +0000

    Creating cookies and Registration form

--- express_server.js
+++ express_server.js
@@ -1,15 +1,44 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cookieParser = require('cookie-parser')
+
+
+
+
+
 
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser())
 function generateRandomString() {
 
-}
+};
+
+// const users = { 
+//   "userRandomID": {
+//     id: "userRandomID", 
+//     email: "user@example.com", 
+//     password: "purple-monkey-dinosaur"
+//   },
+//  "user2RandomID": {
+//     id: "user2RandomID", 
+//     email: "user2@example.com", 
+//     password: "dishwasher-funk"
+//   }
+// };
+
+// const findUsersbyEmail = (email) => {
+//   for(key in users) {
+//     if(users[key].email === email) {
+//       return users[key];
+//     }
+//   }
+//   return null;
+// }
+
 
 
 const urlDatabase = {
@@ -34,8 +63,10 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies.user_id };
+
   res.render("urls_index", templateVars);
+
 });
 
 
@@ -76,3 +107,44 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL
   res.redirect("/urls");
 });
+
+app.get("/login", (req, res) => {
+  res.render("login");
+  
+  res.send(`{$userName}`)
+});
+
+
+app.post("/login", (req, res) => {
+  const cookie = req.body.username;
+  console.log("username", req.body.username)
+  res.cookie("user_id", cookie) 
+  console.log("req.cookie", req.cookies.user_id)
+  res.redirect("/urls")
+});
+
+app.post("/logout", (req, res) => {
+  
+  res.clearCookie("user_id")
+  res.redirect("/urls")
+});
+
+
+
+
+// app.get("/register", (req, res) => {
+//   res.render("register");
+// });
+
+
+//app.post("/register", (req, res) => {
+  // const foundUser = findUserByEmail(req.body.email)
+  //   if(foundUser) {
+  //     res.redirect(`/${foundUser.id}`);
+  //   } else {
+
+  //   }
+
+
+
+
